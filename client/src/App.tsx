b34019a113164c38ba25f26439555e2ff9d9f1df
mben3 <41388158-mben3@users.noreply.replit.com>
2025-05-05 17:36:30 +0000

    Restored to 'debb191f737c20ba66246fe50a54278ee252c61f'
    Replit-Restored-To: debb191f737c20ba66246fe50a54278ee252c61f

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -38,15 +38,11 @@ function ProtectedDashboard() {
 }
 
 function Router() {
-  const basePath = "./"; // Set base path to current directory
-  
   return (
-    <WouterRouter base={basePath}>
-      <Switch>
-        <Route path="/auth" component={AuthPage} />
-        <ProtectedRoute path="/*" component={ProtectedDashboard} />
-      </Switch>
-    </WouterRouter>
+    <Switch>
+      <Route path="/auth" component={AuthPage} />
+      <ProtectedRoute path="/*" component={ProtectedDashboard} />
+    </Switch>
   );
 }
 
